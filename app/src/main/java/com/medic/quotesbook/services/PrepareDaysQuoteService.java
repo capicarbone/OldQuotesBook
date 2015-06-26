003658dbc9aaccbdbb6b041a592d9ddbe42277f3
Carlos Pinelly <cpinelly@gmail.com>
2015-06-26 01:02:43 -0430

    Imagen del autor en la notificación.

--- app/src/main/java/com/medic/quotesbook/services/PrepareDaysQuoteService.java
+++ app/src/main/java/com/medic/quotesbook/services/PrepareDaysQuoteService.java
@@ -8,6 +8,9 @@ import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
@@ -19,6 +22,10 @@ import com.medic.quotesbook.R;
 import com.medic.quotesbook.models.Quote;
 import com.medic.quotesbook.views.activities.BaseActivity;
 import com.medic.quotesbook.views.activities.QuoteActivity;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 /**
  * Created by capi on 27/05/15.
@@ -86,13 +93,36 @@ public class PrepareDaysQuoteService extends IntentService {
 
     }
 
+    private Bitmap getAuthorImage(String imageUrl){
+
+        Bitmap image = null;
+
+        try {
+            URL url = new URL(imageUrl);
+            image = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return image;
+    }
+
     private void giveQuote(Quote quote, Context ctx){
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(ctx);
 
-        builder.setContentTitle("Un mensaje de " + quote.getAuthor().getFullName())
-                .setContentText("Alguien tiene algo que decirte")
+
+        builder.setContentText("Un mensaje de " + quote.getAuthor().getFullName())
+                .setContentTitle("Alguien tiene algo que decirte")
                 .setSmallIcon(R.drawable.ic_launcher);
+
+        if (quote.getAuthor() != null){
+            Bitmap authorImage = getAuthorImage("http://quotesbookapp.appspot.com/" + quote.getAuthor().getPictureUrl());
+
+            builder.setLargeIcon(authorImage);
+        }
 
         Intent intent = new Intent(ctx, QuoteActivity.class);
         intent.putExtra(QuoteActivity.QUOTE_KEY, quote);
