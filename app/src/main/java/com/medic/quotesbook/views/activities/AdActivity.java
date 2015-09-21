38335c63ac9c50b7486dda57899024b8e6b4d50b
Carlos Pinelly <cpinelly@gmail.com>
2015-09-20 21:09:40 -0430

    Separado código de ads en un activity aparte.

---
+++ app/src/main/java/com/medic/quotesbook/views/activities/AdActivity.java
@@ -0,0 +1,135 @@
+package com.medic.quotesbook.views.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.doubleclick.PublisherAdView;
+import com.medic.quotesbook.AppController;
+import com.medic.quotesbook.R;
+import com.medic.quotesbook.utils.AdsKeys;
+import com.tappx.TAPPXAdBanner;
+
+/**
+ * Created by capi on 20/09/15.
+ */
+public class AdActivity extends ActionBarActivity {
+
+    private AppController app;
+
+    private View adWrapper;
+    private AdView adView;
+
+    private PublisherAdView tappxAdBanner = null;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        app = (AppController) getApplication();
+
+    }
+
+    public void initAds(){
+        adWrapper = findViewById(R.id.ad_wrapper);
+        adView = (AdView) findViewById(R.id.ad_view);
+
+        //if (app.isAdsActive()){
+
+        if (isTappxTurn())
+            setupTappxAd();
+        else
+            setupAdMob();
+
+        //}
+    }
+
+    public boolean isTappxTurn(){
+
+        double n = Math.random()*10;
+
+        if (n >= 8.7)
+            return true;
+        else
+            return false;
+    }
+
+    private void setupAdMob(){
+
+        adView = (AdView) findViewById(R.id.ad_view);
+
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice("7DC08B2B34AC3B6CD04D5E05DF311803") // Nexus 5
+                .addTestDevice("95523B02E1B93A6E1B4B82DF09FCE7A5") // Logic X3
+                .build();
+
+        //adView.setAdSize(AdSize.SMART_BANNER);
+        adView.loadAd(adRequest);
+
+        adView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+
+                adWrapper.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                setupTappxAd();
+            }
+        });
+
+
+    }
+
+    public void setupTappxAd(){
+
+        tappxAdBanner = TAPPXAdBanner.ConfigureAndShow(this,
+                tappxAdBanner, AdsKeys.TAPPX_QUOTEVIEW,
+                TAPPXAdBanner.AdPosition.POSITION_BOTTOM, false, new AdListener() {
+                    @Override
+                    public void onAdOpened() {
+                        adWrapper.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(int errorCode) {
+                        setupAdMob();
+                    }
+                });
+
+
+    }
+
+    public AppController getAppCtrl() {
+        return app;
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        if (tappxAdBanner != null)
+            tappxAdBanner.resume();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+        if (tappxAdBanner != null)
+            tappxAdBanner.destroy();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        if (tappxAdBanner != null)
+            tappxAdBanner.pause();
+    }
+
+}
--- app/src/main/java/com/medic/quotesbook/views/activities/QuoteActivity.java
+++ app/src/main/java/com/medic/quotesbook/views/activities/QuoteActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
@@ -35,9 +34,10 @@ import com.medic.quotesbook.views.widgets.RoundedImageNetworkView;
 import com.tappx.TAPPXAdBanner;
 
 
-public class QuoteActivity extends ActionBarActivity {
+public class QuoteActivity extends AdActivity {
 
-    static final String TAG = "QuoteActivity";
+    final String TAG = this.getClass().getSimpleName();
+
     static final String SCREEN_NAME = "Quote Details";
     public static final String SCREEN_NAME_DAYQUOTE = "Day Quote";
 
@@ -48,10 +48,7 @@ public class QuoteActivity extends ActionBarActivity {
     TextView authorNameView;
     RoundedImageNetworkView authorPictureView;
     TextView authorDescriptionView;
-    AdView adView;
 
-    PublisherAdView tappxAdBanner = null;
-    View adWrapper;
 
     private ImageLoader imageLoader = AppController.getInstance().getImageLoader();
 
@@ -59,85 +56,19 @@ public class QuoteActivity extends ActionBarActivity {
 
     Tracker tracker;
 
-    AppController app;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quote);
 
-        app = (AppController) getApplication();
+        initAds();
 
-        tracker = app.getDefaultTracker();
+        tracker = getAppCtrl().getDefaultTracker();
 
-        adWrapper = findViewById(R.id.ad_wrapper);
         quoteBodyView = (TextView) findViewById(R.id.quote_body);
         authorNameView = (TextView) findViewById(R.id.author_name);
         authorDescriptionView = (TextView) findViewById(R.id.author_description);
         authorPictureView = (RoundedImageNetworkView) findViewById(R.id.author_picture);
-
-        if (app.isAdsActive()){
-
-            if (isTappxTurn())
-                setupTappxAd();
-            else
-                setupAd();
-
-        }
-
-    }
-
-    public boolean isTappxTurn(){
-
-        double n = Math.random()*10;
-
-        Log.d(TAG, "Turno: " + Double.toString(n));
-
-        if (n >= 8.5)
-            return true;
-        else
-            return false;
-    }
-
-    public void setupAd(){
-
-        adView = (AdView) findViewById(R.id.ad_view);
-
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice("7DC08B2B34AC3B6CD04D5E05DF311803") // Nexus 5
-                .addTestDevice("95523B02E1B93A6E1B4B82DF09FCE7A5") // Logic X3
-                .build();
-
-        //adView.setAdSize(AdSize.SMART_BANNER);
-        adView.loadAd(adRequest);
-
-        adView.setAdListener(new AdListener() {
-            @Override
-            public void onAdLoaded() {
-                super.onAdLoaded();
-
-                adWrapper.setVisibility(View.VISIBLE);
-            }
-
-            @Override
-            public void onAdFailedToLoad(int errorCode) {
-                setupTappxAd();
-            }
-        });
-
-
-    }
-
-    public void setupTappxAd(){
-
-        tappxAdBanner = TAPPXAdBanner.ConfigureAndShow(this,
-                tappxAdBanner, AdsKeys.TAPPX_QUOTEVIEW,
-                TAPPXAdBanner.AdPosition.POSITION_BOTTOM, false, new AdListener(){
-                    @Override public void onAdOpened() {
-                        adWrapper.setVisibility(View.VISIBLE);
-                    }
-                });
-
 
     }
 
@@ -229,7 +160,7 @@ public class QuoteActivity extends ActionBarActivity {
 
         // If no Ads
 
-        if (!app.isAdsActive()){
+        if (!getAppCtrl().isAdsActive()){
 
             //// dp to px
             Resources r = this.getResources();
@@ -264,24 +195,6 @@ public class QuoteActivity extends ActionBarActivity {
 
         tracker.send(new HitBuilders.EventBuilder().build());
 
-        if (tappxAdBanner != null)
-            tappxAdBanner.resume();
-
-    }
-
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-
-        if (tappxAdBanner != null)
-            tappxAdBanner.destroy();
-    }
-
-    protected void onPause(){
-        super.onPause();
-
-        if (tappxAdBanner != null)
-            tappxAdBanner.pause();
     }
 
 
